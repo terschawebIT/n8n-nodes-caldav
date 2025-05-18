7f4d3c2ea1a178504634a0b41f65caccfd3fb334
Niko Terschawetz <niko.terschawetz@digitalnativealliance.com>
2025-05-18 20:42:32 +0200

    fix: Verbesserte Dependency-Handhabung

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 const path = require('path');
-const { task, src, dest } = require('gulp');
+const { task, src, dest, series } = require('gulp');
 
-task('build:icons', copyIcons);
+task('copy:package', function() {
+	return src(['package.json'])
+		.pipe(dest('dist'));
+});
+
+task('build:icons', function() {
+	return src('nodes/**/*.svg')
+		.pipe(dest('dist/nodes'));
+});
+
+task('build', series('build:icons', 'copy:package'));
 
 function copyIcons() {
 	const nodeSource = path.resolve('nodes', '**', '*.{png,svg}');
